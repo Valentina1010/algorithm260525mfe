d0a51d36e6f61e0c8f2cd74bb0efa2745e0fd4f1
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2025-09-02 16:31:59 +0300

    lesson6

---
+++ lesson_6_20250902/quick_sort.js
@@ -0,0 +1,37 @@
+/** Быстрая сортировка */
+
+function quickSort(array, start, end) {
+    if (start < end) {
+        let indexPivot = partition(array, start, end);
+        // меньшие элементы чем опорный размещаем слева
+        quickSort(array, start, indexPivot - 1);
+        // большие элементы чем опорный размещаем справа
+        quickSort(array, indexPivot + 1, end);
+    }
+}
+
+function partition(array, start, end) {
+    
+    let pivot = array[end]; // выбираем опорный элемент
+    let indexPivot = start; // Индекс первого элемента для сравнения с опорным
+
+    for (let i = start; i < end; i++) {
+        if (array[i] >= pivot) {
+            swap(array, i, indexPivot);
+            indexPivot++;
+        }
+    }
+    swap(array, end, indexPivot); // перемещает опорный элемент в правильную позицию в отсортированном массиве
+
+    return indexPivot;
+}
+
+function swap(array, first, second) {
+    let temp = array[first];
+    array[first] = array[second];
+    array[second] = temp;
+}
+
+let testArray = [7, -2, 4, 1, 6, 5, 0, -4, 2];
+quickSort(testArray, 0, testArray.length - 1);
+console.log(testArray);
